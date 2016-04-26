79ecd76ad876d49b43401e89efa178c8b58e148a
Michael Burke <mike.d.burke@gmail.com>
2016-04-25 22:04:20 -0400

    'The Basics' section

---
+++ basics/Example3.rb
@@ -0,0 +1,7 @@
+# Program that stores list of movie titles and year they came out.
+
+movies = { :'The Shawshank Redemption' => 1994, :'The Godfather' => 1972, :'The Dark Knight' => 2008 }
+
+puts movies[:'The Shawshank Redemption']
+puts movies[:'The Godfather']
+puts movies[:'The Dark Knight']
---
+++ basics/Example6.rb
@@ -0,0 +1,5 @@
+square1 = 3.5 * 3.5
+square2 = 75.4 * 75.4
+square3 = 2.2 * 2.2
+
+puts square1, square2, square3
