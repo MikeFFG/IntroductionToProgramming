629305f58cae4c35b0d310b824259984cdaf660a
Michael 'da boss' Burke <mburke@izotope.com>
2016-04-26 15:33:30 -0400

    Completed the Methods chapter.

---
+++ Methods/Example1.rb
@@ -0,0 +1,8 @@
+# Write a program that prints a greeting message. 
+# This program should contain a method called greeting that takes a name as its parameter and returns a string.
+
+def greeting(name="Bob")
+	puts "Hello #{name}!"
+end
+
+greeting("Dave")
